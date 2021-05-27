e058c95835faf2e9c05b82c46e63bf050e086acd
iliynsd <75386833+iliynsd@users.noreply.github.com>
2021-05-27 21:46:27 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MongoDB.Driver;
+
+namespace WpfAppMongoDB
+{
+   
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void Button_Add_Click(object sender, RoutedEventArgs e)
+        {
+            AddWindow addWindow = new AddWindow();
+            // addWindow.Owner = this;
+            addWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Change_Click(object sender, RoutedEventArgs e)
+        {
+            FindWindow findWindow = new FindWindow();
+            findWindow.Show();
+            this.Close();
+
+        }
+
+        private void Button_Search_Click(object sender, RoutedEventArgs e)
+        {
+            FindWindow findWindow = new FindWindow();
+            findWindow.Show();
+            this.Close();
+
+        }
+    }
+
+    
+}
---
+++ WpfAppMongoDB/AddWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using System.Windows;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+namespace WpfAppMongoDB
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        const string connection_string = "mongodb://localhost";
+        private MongoClient db = new MongoClient(connection_string);
+        public AddWindow()
+        {
+            InitializeComponent();
+        }
+
+        class Disks
+        {
+            
+           public string name { get; set; }
+           public string executor { get; set; }
+           public  string style { get; set; }
+           public string creator { get; set; } 
+           public string date { get; set; }
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            var test = db.GetDatabase("test");
+            var disks = test.GetCollection<Disks>("disks");
+
+            var temp = new Disks
+            {
+                name = Disk.Text,
+                executor = Executor.Text,
+                style = Style.Text,
+                creator = Creator.Text,
+                date = Date.Text
+            };
+
+            
+
+             disks.InsertOne(temp);
+           
+ 
+ MainWindow mainWindow = new MainWindow();
+              mainWindow.Show();
+             this.Close();
+             
+        }
+
+
+
+    }
+}
